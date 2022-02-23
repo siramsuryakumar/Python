2b93c90198ea04e4a00946af42417370c11fc51d
siramsuryakumar <53056583+siramsuryakumar@users.noreply.github.com>
2022-02-23 10:06:07 +0530

    Python basics

---
+++ Variables-Assignments-exercise.py
@@ -0,0 +1,9 @@
+int_var = 5
+print(int_var)
+float_var = 1.11
+boolean_var = True
+int_var = 10
+print(int_var)
+print(float_var)
+print(boolean_var)
+print("Successful")
---
+++ Variables-Assignments.py
@@ -0,0 +1,4 @@
+ex_variable = 5
+print(ex_variable)
+ex_variable = 7
+print(ex_variable)
